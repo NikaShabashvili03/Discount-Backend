c3b7c75d75c1b3c337fd456f57e56c75356c6297
Kakhi Kakhidze <kakhikakhidze3@gmail.com>
2026-05-21 22:30:51 +0400

    new

--- orders/models/payment.py
+++ orders/models/payment.py
@@ -36,7 +36,7 @@ class Payment(models.Model):
         related_name='payment'
     )
 
-    payment_method = models.CharField(max_length=20, choices=PAYMENT_METHODS)
+    payment_method = models.CharField(max_length=20)
     amount = models.DecimalField(max_digits=10, decimal_places=2, default=0) 
     requested_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     refund_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
